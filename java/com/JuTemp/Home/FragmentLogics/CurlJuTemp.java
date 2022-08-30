76dd4897ecbbe82ad391af8bc9a512ae530c72ff
JuTemp <jtp0415@outlook.com>
2022-08-30 15:33:50 +0800

    Home20220830

--- java/com/JuTemp/Home/FragmentLogics/CurlJuTemp.java
+++
@@ -1,5 +0,0 @@
-package com.JuTemp.Home.FragmentLogics;
-
-import com.JuTemp.Home.util.FragmentLogicJuTemp;
-
-public class CurlJuTemp extends FragmentLogicJuTemp {}
--- java/com/JuTemp/Home/util/NumberJuTemp.java
+++
@@ -1,9 +0,0 @@
-package com.JuTemp.Home.util;
-
-public class NumberJuTemp
-{
-	public static String showSign(int p)
-	{
-		return p > 0 ? String.valueOf("+" + p) : String.valueOf(p);
-	}
-}
